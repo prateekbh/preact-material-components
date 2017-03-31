099f70ba3f7f0ea45c97de0bee1c98e7b1ce4a9e
Prateek Bhatnagar <prateek89born@gmail.com>
2017-03-31 11:42:13 +0530

    adding css to bundles

--- copy-css.js
+++ copy-css.js
@@ -1,5 +1,5 @@
 /*globals require, __dirname*/
-const cp = require('glob-cp');
+const cp = require('cp');
 const path = require('path');
 
 const srcPath = path.join(__dirname, 'node_modules', '@material');
@@ -27,8 +27,8 @@ const bundleMapping = {
 for (let dest in bundleMapping) {
 	if (bundleMapping.hasOwnProperty(dest)) {
 		const source = bundleMapping[dest];
-		const sourcePath = path.join(srcPath, source, 'dist', '*.css');
-		const destPath = path.join(__dirname, 'Components', dest, '*.css');
+		const sourcePath = path.join(srcPath, source, 'dist', 'mdc.' + source + '.css');
+		const destPath = path.join(__dirname, 'Components', dest, dest + '.css');
 		cp.sync(sourcePath, destPath);
 	}
 }
