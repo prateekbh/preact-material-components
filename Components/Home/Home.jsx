afec759cdb02edffd873deeb86cee108fb2c2bff
Prateek Bhatnagar <prateek89born@gmail.com>
2017-04-02 09:10:38 +0530

    setting up drawer

--- Components/Home/Home.jsx
+++ Components/Home/Home.jsx
@@ -1,15 +1,25 @@
 import {h, Component} from 'preact';
 import Icon from 'preact-material-components/Icon';
 import Toolbar from 'preact-material-components/Toolbar';
+import Drawer from 'preact-material-components/Drawer';
 import 'preact-material-components/Icon/Icon.css';
 import 'preact-material-components/Toolbar/Toolbar.css';
+import 'preact-material-components/Drawer/Drawer.css';
 import './Home.css';
 export default class Home extends Component {
 	render(){
 		return (
 			<div className="home">
 				<div className="hero">
-					<Toolbar icon="menu" />
+					<Toolbar>
+						<Toolbar.Section align-start={true}>
+							<Toolbar.Icon href="#" onClick={(e)=>{
+									e.preventDefault();
+									this.drawer.MDComponent.open = true;
+								}}>menu</Toolbar.Icon>
+						</Toolbar.Section>
+					</Toolbar>
+					<Drawer.TemporaryDrawer ref={drawer=>{this.drawer = drawer;}} />
 				</div>
 			</div>
 		);
