c5260dadf344f700dc74035930d03c2c7b45c3e7
Prateek Bhatnagar <prateek89born@gmail.com>
2017-04-05 19:01:31 +0530

    disabling switch (#53)

--- Switch/index.js
+++ Switch/index.js
@@ -13,7 +13,7 @@ export default class Switch extends MaterialComponent {
 	materialDom(props) {
 		return (
 			<div {...props}>
-				<input type="checkbox" id="basic-switch" class="mdc-switch__native-control" />
+				<input type="checkbox" id="basic-switch" class="mdc-switch__native-control" disabled={props.disabled} />
 				<div class="mdc-switch__background">
 					<div class="mdc-switch__knob"></div>
 				</div>
