5e35ebeff10cbad9af2d17bb75b9419e201a7f08
Prateek Bhatnagar <prateek89born@gmail.com>
2017-05-19 12:03:44 +0530

    tab page demo

--- Components/TabsPage/TabsPage.jsx
+++ Components/TabsPage/TabsPage.jsx
@@ -7,77 +7,96 @@ import sample from './sample.txt';
 import './TabsPage.css';
 export default class TabsPage
  extends Component {
-	constructor() {
-		super();
-		this.propsTable = [
-			{
-				component: 'Tabs',
-				props: [
-					{
-						name: 'fixed',
-						description: 'Makes the toolbar fixed'
-					}
-				]
-			},
-		];
-	}
-	render(){
-		return (
-			<div className="page-tabs">
-				<ComponentTable data={this.propsTable} />
+  constructor() {
+    super();
+    this.propsTable = [
+      {
+        component: 'Tabs',
+        props: [
+          {
+            name: 'indicator-accent',
+            description: 'Makes the indicator accented.'
+          },
+          {
+            name: 'icon-tab-bar',
+            description: 'Makes the tab bar accomodate icons.'
+          },
+          {
+            name: 'icons-with-text',
+            description: 'Makes the tab bar accomodate icons and label.'
+          }
+        ]
+      },
+      {
+        component: 'Tab',
+        props: [
+          {
+            name: 'active',
+            description: 'Makes the current tab active.'
+          },
+        ]
+      },
+    ];
+  }
+  render(){
+    return (
+      <div className="page-tabs">
+        <ComponentTable data={this.propsTable} />
 
-				<div className="mdc-typography--display1">Sample code </div>
-				<CodeBlock>
-					<code class='lang-js' >
-						{sample}
-					</code>
-				</CodeBlock>
+        <div className="mdc-typography--display1">Sample code </div>
+        <CodeBlock>
+          <code class='lang-js' >
+            {sample}
+          </code>
+        </CodeBlock>
 
-				<div className="mdc-typography--display1">Original documentation</div>
-				<div className="mdc-typography--body">
-					This component encapsulates <span className="strong">mdc-tabs</span>, you can refer to its documentation
-					<a href="https://github.com/material-components/material-components-web/tree/master/packages/mdc-tabs"> here</a>.
-				</div>
+        <div className="mdc-typography--display1">Original documentation</div>
+        <div className="mdc-typography--body">
+          This component encapsulates <span className="strong">mdc-tabs</span>, you can refer to its documentation
+          <a href="https://github.com/material-components/material-components-web/tree/master/packages/mdc-tabs"> here</a>.
+        </div>
 
-				<div className="mdc-typography--display1">Demo </div>
-				<div className="mdc-typography--title">Default </div>
-					<Tabs className='demo-tabs'>
-						<Tabs.Tab>Tab1</Tabs.Tab>
-						<Tabs.Tab>Tab2</Tabs.Tab>
-						<Tabs.Tab>Tab3</Tabs.Tab>
-					</Tabs>
-				<div className="mdc-typography--title">With indicator accent </div>
-					<Tabs className='demo-tabs' indicator-accent={true}>
-						<Tabs.Tab>Tab1</Tabs.Tab>
-						<Tabs.Tab>Tab2</Tabs.Tab>
-						<Tabs.Tab>Tab3</Tabs.Tab>
-					</Tabs>
-				<div className="mdc-typography--title">Icons tabs bar </div>
-					<Tabs className='demo-tabs' icon-tab-bar={true}>
-						<Tabs.Tab>
-							<Icon>favorite</Icon>
-						</Tabs.Tab>
-						<Tabs.Tab>
-							<Icon>done</Icon>
-						</Tabs.Tab>
-						<Tabs.Tab>
-							<Icon>info</Icon>
-						</Tabs.Tab>
-					</Tabs>
-				<div className="mdc-typography--title">Icons with test tabs bar </div>
-					<Tabs className='demo-tabs' icons-with-text={true}>
-						<Tabs.Tab>
-							<Icon>favorite</Icon>
-							Hello
-						</Tabs.Tab>
-						<Tabs.Tab active={true}>
-							<Icon>done</Icon>
-						</Tabs.Tab>
-						<Tabs.Tab>
-							<Icon>info</Icon>
-						</Tabs.Tab>
-					</Tabs>
-			</div>
-		);
-	}
+        <div className="mdc-typography--display1">Demo </div>
+        <div className="mdc-typography--title">Default </div>
+          <Tabs className='demo-tabs'>
+            <Tabs.Tab>Tab1</Tabs.Tab>
+            <Tabs.Tab>Tab2</Tabs.Tab>
+            <Tabs.Tab>Tab3</Tabs.Tab>
+          </Tabs>
+        <div className="mdc-typography--title">With indicator accent </div>
+          <Tabs className='demo-tabs' indicator-accent={true}>
+            <Tabs.Tab>Tab1</Tabs.Tab>
+            <Tabs.Tab>Tab2</Tabs.Tab>
+            <Tabs.Tab>Tab3</Tabs.Tab>
+          </Tabs>
+        <div className="mdc-typography--title">Icons tabs bar </div>
+          <Tabs className='demo-tabs' icon-tab-bar={true}>
+            <Tabs.Tab>
+              <Icon>favorite</Icon>
+            </Tabs.Tab>
+            <Tabs.Tab>
+              <Icon>done</Icon>
+            </Tabs.Tab>
+            <Tabs.Tab>
+              <Icon>info</Icon>
+            </Tabs.Tab>
+          </Tabs>
+        <div className="mdc-typography--title">Icons with test tabs bar </div>
+          <Tabs className='demo-tabs' icons-with-text={true}>
+            <Tabs.Tab>
+              <Icon>favorite</Icon>
+              <Tabs.TabIconLabel>Heart</Tabs.TabIconLabel>
+            </Tabs.Tab>
+            <Tabs.Tab active={true}>
+              <Icon>done</Icon>
+              <Tabs.TabIconLabel>Done</Tabs.TabIconLabel>
+            </Tabs.Tab>
+            <Tabs.Tab>
+              <Icon>info</Icon>
+              <Tabs.TabIconLabel>Info</Tabs.TabIconLabel>
+            </Tabs.Tab>
+          </Tabs>
+      </div>
+    );
+  }
 }
