9a467661aec978ec25f6bc6cfd1d90141ad0faec
Prateek Bhatnagar <prateek89born@gmail.com>
2017-07-18 14:27:23 +0530

    toggle icon fix (#178)

--- IconToggle/IconToggle.jsx
+++ IconToggle/IconToggle.jsx
@@ -1,4 +1,5 @@
 import { h } from "preact";
+import { MDCIconToggle } from "@material/icon-toggle/";
 import MaterialComponent from "../MaterialComponent";
 
 /**
@@ -9,9 +10,25 @@ export default class IconToggle extends MaterialComponent {
     super();
     this.componentName = "icon-toggle";
   }
+  componentDidMount() {
+    this.MDComponent = new MDCIconToggle(this.control);
+  }
+  componentWillUnmount() {
+    this.MDComponent.destroy && this.MDComponent.destroy();
+  }
   materialDom(props) {
+    if (props["data-toggle-on"])
+      props["data-toggle-on"] = JSON.stringify(props["data-toggle-on"]);
+    if (props["data-toggle-off"])
+      props["data-toggle-off"] = JSON.stringify(props["data-toggle-off"]);
     return (
-      <i {...props} className="material-icons">
+      <i
+        {...props}
+        className="material-icons"
+        ref={control => {
+          this.control = control;
+        }}
+      >
         {props.children}
       </i>
     );
