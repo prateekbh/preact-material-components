b8d8b4dc5b5f6c60db536eead83c3d0050f8ec5f
Philippe Schommers <philippe@schommers.be>
2017-07-23 23:42:44 +0200

    Add basic helper text

--- Textfield/Textfield.jsx
+++ Textfield/Textfield.jsx
@@ -3,6 +3,25 @@ import MaterialComponent from "../MaterialComponent";
 import { MDCTextfield } from "@material/textfield/";
 
 /**
+ * @prop persistent = false
+ * @prop validation-msg = false
+ */
+class Helptext extends MaterialComponent {
+  constructor() {
+    super();
+    this.componentName = "textfield-helptext";
+    this._mdcProps = ["persistent", "validation-msg"];
+  }
+  materialDom(props) {
+    return (
+      <p {...props} aria-hidden="true">
+        {props.children}
+      </p>
+    );
+  }
+}
+
+/**
  * @prop fullwidth = false
  * @prop multiline = false
  * @prop dense = false
@@ -12,7 +31,7 @@ import { MDCTextfield } from "@material/textfield/";
 export default class Textfield extends MaterialComponent {
   constructor() {
     super();
-    this.componentName = "textfield";
+    this.componentName = "";
     this._mdcProps = ["fullwidth", "multiline", "dense", "disabled"];
   }
   componentDidMount() {
@@ -22,24 +41,51 @@ export default class Textfield extends MaterialComponent {
     this.MDComponent.destroy && this.MDComponent.destroy();
   }
   materialDom(allprops) {
-    const { className, ...props } = allprops;
-    let labelClass = ["mdc-textfield__label"];
+    const {
+      className,
+      helptextPersistent,
+      helptextValidationMsg,
+      ...props
+    } = allprops;
+
+    // Helper text
+    const helptextProps = {
+      persistent: helptextPersistent,
+      "validation-msg": helptextValidationMsg
+    };
+
+    // Label
+    const labelClass = ["mdc-textfield__label"];
     if (props.value) {
       labelClass.push("mdc-textfield__label--float-above");
     }
+
+    // Component
+    const inputClass = ["mdc-textfield"];
+    if (className) {
+      inputClass.push(className);
+    }
+
     return (
-      <div className={className + ""} ref={control => (this.control = control)}>
-        {props.multiline
-          ? <textarea className="mdc-textfield__input" {...props} />
-          : <input
-              type={props.type || "text"}
-              className="mdc-textfield__input"
-              {...props}
-            />}
-        {props.label &&
-          <label className={labelClass.join(" ")}>
-            {props.label}
-          </label>}
+      <div>
+        <div className={inputClass} ref={control => (this.control = control)}>
+          {props.multiline
+            ? <textarea className="mdc-textfield__input" {...props} />
+            : <input
+                type={props.type || "text"}
+                className="mdc-textfield__input"
+                aria-controls={props.id + "-helptext"}
+                {...props}
+              />}
+          {props.label &&
+            <label className={labelClass.join(" ")}>
+              {props.label}
+            </label>}
+        </div>
+        {props.helptext &&
+          <Helptext id={props.id + "-helptext"} {...helptextProps}>
+            {props.helptext}
+          </Helptext>}
       </div>
     );
   }
