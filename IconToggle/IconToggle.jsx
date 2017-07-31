0dd7f51f87c2ecd93ce259ae00dedc24bef76940
Paul Martin <ukrcamilio@gmail.com>
2017-07-31 13:51:28 -0400

    Added in onChange event and className extending (#183)
    * Added in onChange event and className extending
    
    * Remove listeners before destroying component

--- IconToggle/IconToggle.jsx
+++ IconToggle/IconToggle.jsx
@@ -9,11 +9,19 @@ export default class IconToggle extends MaterialComponent {
   constructor() {
     super();
     this.componentName = "icon-toggle";
+    this._onChange = this._onChange.bind(this);
+  }
+  _onChange(e) {
+    if (this.props.onChange) {
+      this.props.onChange(e);
+    }
   }
   componentDidMount() {
     this.MDComponent = new MDCIconToggle(this.control);
+    this.MDComponent.listen("MDCIconToggle:change", this._onChange);
   }
   componentWillUnmount() {
+    this.MDComponent.unlisten("MDCIconToggle:change", this._onChange);
     this.MDComponent.destroy && this.MDComponent.destroy();
   }
   materialDom(props) {
@@ -24,7 +32,7 @@ export default class IconToggle extends MaterialComponent {
     return (
       <i
         {...props}
-        className="material-icons"
+        className={"material-icons " + props.className || ""}
         ref={control => {
           this.control = control;
         }}
