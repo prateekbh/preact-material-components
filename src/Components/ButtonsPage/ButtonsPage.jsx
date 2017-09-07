ec99136e90a8cbc55d64b9295007c726cdc69830
Prateek Bhatnagar <prateek89born@gmail.com>
2017-09-07 15:59:51 +0530

    updating documentation (#244)

--- src/Components/ButtonsPage/ButtonsPage.jsx
+++ src/Components/ButtonsPage/ButtonsPage.jsx
@@ -31,6 +31,18 @@ export default class ButtonsPage extends Component {
 					{
 						name: 'compact',
 						description: 'Adds compact padding to the button'
+					},
+					{
+						name: 'raised',
+						description: 'Adds raised effect to the button'
+					},
+					{
+						name: 'unelevated',
+						description: 'Adds unelevated effect to the button'
+					},
+					{
+						name: 'stroked',
+						description: 'Adds stroked effect to the button'
 					}
 				]
 			}
@@ -58,27 +70,43 @@ export default class ButtonsPage extends Component {
 				<div className="mdc-typography--display1">Demo </div>
 				<div className="mdc-typography--title">Flats </div>
 				<Button>Flat button without ripple</Button>
-				<Button ripple={true}>Flat button with ripple</Button>
-				<Button accent={true}>Accented Flat button</Button>
-				<Button dense={true}>Dense Flat button </Button>
-				<Button compact={true}>Compact Flat button </Button>
-				<Button primary={true}>Primary Flat button </Button>
+				<Button ripple>Flat button with ripple</Button>
+				<Button accent>Accented Flat button</Button>
+				<Button dense>Dense Flat button </Button>
+				<Button compact>Compact Flat button </Button>
+				<Button primary>Primary Flat button </Button>
 
 				<div className="mdc-typography--title">Raised </div>
-				<Button raised={true}>Raised button without ripple</Button>
-				<Button raised={true} ripple={true}>Raised button with ripple</Button>
-				<Button raised={true} accent={true}>Accented Raised button</Button>
-				<Button raised={true} dense={true}>Dense Raised button </Button>
-				<Button raised={true} compact={true}>Compact Raised button </Button>
-				<Button raised={true} primary={true}>Primary Raised button </Button>
+				<Button raised>Raised button without ripple</Button>
+				<Button raised ripple>Raised button with ripple</Button>
+				<Button raised accent>Accented Raised button</Button>
+				<Button raised dense>Dense Raised button </Button>
+				<Button raised compact>Compact Raised button </Button>
+				<Button raised primary>Primary Raised button </Button>
+
+				<div className="mdc-typography--title">Unelevated </div>
+				<Button unelevated>Unelevated button without ripple</Button>
+				<Button unelevated ripple>Unelevated button with ripple</Button>
+				<Button unelevated accent>Accented Unelevated button</Button>
+				<Button unelevated dense>Dense Unelevated button </Button>
+				<Button unelevated compact>Compact Unelevated button </Button>
+				<Button unelevated primary>Primary Unelevated button </Button>
+
+				<div className="mdc-typography--title">Stroked </div>
+				<Button stroked>Stroked button without ripple</Button>
+				<Button stroked ripple>Stroked button with ripple</Button>
+				<Button stroked accent>Accented Stroked button</Button>
+				<Button stroked dense>Dense Stroked button </Button>
+				<Button stroked compact>Compact Stroked button </Button>
+				<Button stroked primary>Primary Stroked button </Button>
 
 				<div className="mdc-typography--title">Disabled </div>
-				<Button disabled={true}>Disabled button without ripple</Button>
-				<Button disabled={true} ripple={true}>Disabled button with ripple</Button>
-				<Button disabled={true} accent={true}>Accented Disabled button</Button>
-				<Button disabled={true} dense={true}>Dense Disabled button </Button>
-				<Button disabled={true} compact={true}>Compact Disabled button </Button>
-				<Button disabled={true} primary={true}>Primary Disabled button </Button>
+				<Button disabled>Disabled button without ripple</Button>
+				<Button disabled ripple>Disabled button with ripple</Button>
+				<Button disabled accent>Accented Disabled button</Button>
+				<Button disabled dense>Dense Disabled button </Button>
+				<Button disabled compact>Compact Disabled button </Button>
+				<Button disabled primary>Primary Disabled button </Button>
 
 				<div className="mdc-typography--title">Note</div>
 				<div className="mdc-typography--caption">
