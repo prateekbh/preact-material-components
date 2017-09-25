2205df0f38a1ba7aa380826b536d0cb9eb2d42f7
Prateek Bhatnagar <prateek89born@gmail.com>
2017-09-25 19:12:59 +0530

    Migrate21 (#276)
    * fix(package): update material-components-web to version 0.21.0 (#262)
    
    * adding scss to npm package
    
    * fixes fab for mdc0.21.0 (#267)
    
    * adding scss to npm package (#268)
    
    * fixing button for mdc0.21.0 (#266)
    
    * fixing button for mdc0.21.0
    
    * Update Button.jsx
    
    * Snackbar fix (#272)
    
    * adding scss to npm package
    
    * removing mdc button dep from snackbard
    
    * new version
    
    * fixing CssMigrationPlugin
    
    * fixing deps
    
    * bumping version

--- Button/Button.jsx
+++ Button/Button.jsx
@@ -1,28 +1,20 @@
 import { h } from "preact";
 import MaterialComponent from "../MaterialComponent";
+import Icon from "../Icon/";
 
 /**
  *  @prop dense = false
  *  @prop raised = false
  *  @prop compact = false
- *  @prop primary = false
- *  @prop accent = false
  *  @prop disabled = false
  *  @prop unelevated = false
+ *  @prop stroked = false
  */
-export default class Button extends MaterialComponent {
+class Button extends MaterialComponent {
   constructor() {
     super();
     this.componentName = "button";
-    this._mdcProps = [
-      "dense",
-      "raised",
-      "compact",
-      "primary",
-      "accent",
-      "unelevated",
-      "stroked"
-    ];
+    this._mdcProps = ["dense", "raised", "compact", "unelevated", "stroked"];
   }
   componentDidMount() {
     super.attachRipple();
@@ -42,3 +34,13 @@ export default class Button extends MaterialComponent {
     );
   }
 }
+
+class ButtonIcon extends Icon {
+  constructor() {
+    super();
+    this.componentName = "button__icon";
+  }
+}
+
+Button.Icon = ButtonIcon;
+export default Button;
