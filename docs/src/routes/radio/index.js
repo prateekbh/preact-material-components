a96449711f9eb43fd984aecba1abbd1095f12d06
mellogarrett <garrett6346@gmail.com>
2017-10-30 13:27:48 -0400

    radio code examples

--- docs/src/routes/radio/index.js
+++ docs/src/routes/radio/index.js
@@ -11,6 +11,10 @@ import CodeBlock from "../../components/code-block";
 
 // Samples
 import sample from "./sample.txt";
+import basicExample from "./basic-example.txt";
+import disabledExample from "./disabled-example.txt";
+import controlledExample from "./controlled-example.txt";
+import advancedControlledExample from "./advanced-controlled-example.txt";
 
 // Class
 export default class RadioPage extends Component {
@@ -36,12 +40,10 @@ export default class RadioPage extends Component {
     return (
       <div>
         <ComponentTable data={this.propsTable} />
-
         <div className="mdc-typography--display1">Sample code </div>
         <CodeBlock>
           <code class="lang-js">{sample}</code>
         </CodeBlock>
-
         <div className="mdc-typography--display1">Original documentation</div>
         <div className="mdc-typography--body">
           This component encapsulates <span className="strong">mdc-radio</span>,
@@ -51,20 +53,71 @@ export default class RadioPage extends Component {
             here
           </a>.
         </div>
-
         <div className="mdc-typography--display1">Demo </div>
-        <Formfield>
-          <Radio id="r1" name="opts" />
-          <label for="r1">Radio 1</label>
-        </Formfield>
-        <Formfield>
-          <Radio id="r2" name="opts" />
-          <label for="r2">Radio 2</label>
-        </Formfield>
-        <Formfield>
-          <Radio id="r3" name="opts" disabled={true} />
-          <label for="r3">Disabled Radio </label>
-        </Formfield>
+        <div className="mdc-typography--title">Basic radio group</div>
+        <div>
+          <Formfield>
+            <Radio id="radio-1" name="Basic Options" />
+            <label for="radio-1">Radio 1</label>
+          </Formfield>
+          <Formfield>
+            <Radio id="radio-2" name="Basic Options" />
+            <label for="radio-2">Radio 2</label>
+          </Formfield>
+          <Formfield>
+            <Radio id="radio-3" name="Basic Options" />
+            <label for="radio-3">Radio 3</label>
+          </Formfield>
+        </div>
+        <CodeBlock>
+          <code class="lang-html">{basicExample}</code>
+        </CodeBlock>
+        <div className="mdc-typography--title">
+          Radio group with disabled option
+        </div>
+        <div>
+          <Formfield>
+            <Radio disabled={true} id="radio-1" name="Disabled Options" />
+            <label for="radio-1">Disabled</label>
+          </Formfield>
+          <Formfield>
+            <Radio id="radio-2" name="Disabled Options" />
+            <label for="radio-2">Radio 2</label>
+          </Formfield>
+          <Formfield>
+            <Radio id="radio-3" name="Disabled Options" />
+            <label for="radio-3">Radio 3</label>
+          </Formfield>
+        </div>
+        <CodeBlock>
+          <code class="lang-html">{disabledExample}</code>
+        </CodeBlock>
+        <div className="mdc-typography--title">Controlled radio group</div>
+        <div>
+          <Formfield>
+            <Radio id="radio-1" name="Controlled Options" />
+            <label for="radio-1">Radio 1</label>
+          </Formfield>
+          <Formfield>
+            <Radio checked={true} id="radio-2" name="Controlled Options" />
+            <label for="radio-2">Radio 2</label>
+          </Formfield>
+          <Formfield>
+            <Radio id="radio-3" name="Controlled Options" />
+            <label for="radio-3">Radio 3</label>
+          </Formfield>
+        </div>
+        <CodeBlock>
+          <code class="lang-html">{controlledExample}</code>
+        </CodeBlock>
+        <div className="mdc-typography--title">
+          Advanced controlled radio group
+        </div>
+        This is a possible example of controlling the values of a group of radio
+        buttons in component state.
+        <CodeBlock>
+          <code class="lang-js">{advancedControlledExample}</code>
+        </CodeBlock>
       </div>
     );
   }
