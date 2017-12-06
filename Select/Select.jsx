c5c49961adc0537193590928b80182d4a6ed6ea3
mellogarrett <garrett6346@gmail.com>
2017-12-06 12:18:54 -0500

    updated comparison operator

--- Select/Select.jsx
+++ Select/Select.jsx
@@ -43,7 +43,10 @@ class Select extends MaterialComponent {
     this.updateSelection();
     if (this.MDComponent && this.MDComponent.foundation_) {
       this.MDComponent.foundation_.resize();
-      if (this.props.selectedIndex == null || this.props.selectedIndex == -1) {
+      if (
+        this.props.selectedIndex === null ||
+        this.props.selectedIndex === -1
+      ) {
         this.MDComponent.foundation_.adapter_.setSelectedTextContent(
           this.props.hintText
         );
