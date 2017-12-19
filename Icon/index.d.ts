f1110912c6e134aa47842d4f2b0991b620c97393
Carl Vuorinen <carl.vuorinen@gmail.com>
2017-12-19 23:23:31 +0200

    Remove redundant prop `disabled` (inherited from HTMLAttributes)

--- Icon/index.d.ts
+++ Icon/index.d.ts
@@ -1,7 +1,5 @@
 import MaterialComponent from '../MaterialComponent';
 import { VNode } from 'preact';
 
-declare interface IIconProps extends JSX.HTMLAttributes {
-  disabled?: boolean;
-}
+declare interface IIconProps extends JSX.HTMLAttributes {}
 export default class Icon extends MaterialComponent<IIconProps, {}> {}
