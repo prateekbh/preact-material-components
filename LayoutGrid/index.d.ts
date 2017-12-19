0d80d85f99495be1c7d342dcf722ee99a75ebb38
Carl Vuorinen <carl.vuorinen@gmail.com>
2017-12-19 23:56:21 +0200

    Add more specific types for LayoutGridCell

--- LayoutGrid/index.d.ts
+++ LayoutGrid/index.d.ts
@@ -8,12 +8,16 @@ export default class LayoutGrid extends MaterialComponent<JSX.HTMLAttributes, {}
 
 declare class LayoutGridInner extends MaterialComponent<JSX.HTMLAttributes, {}> {}
 
+type PhoneCols = 1 | 2 | 3 | 4;
+type TabletCols = PhoneCols | 5 | 6 | 7 | 8;
+type LayoutCols = TabletCols | 9 | 10 | 11 | 12;
+
 declare interface ILayoutGridCellProps extends JSX.HTMLAttributes {
-  cols?: number;
-  desktopCols?: number;
-  tabletCols?: number;
-  phoneCols?: number;
-  order?: number;
-  align?: string;
+  cols?: LayoutCols;
+  desktopCols?: LayoutCols;
+  tabletCols?: TabletCols;
+  phoneCols?: PhoneCols;
+  order?: LayoutCols;
+  align?: 'top' | 'middle' | 'bottom';
 }
 declare class LayoutGridCell extends MaterialComponent<ILayoutGridCellProps, {}> {}
