ed499961e2e02bf18928be09cf785ea76702cedd
Carl Vuorinen <carl.vuorinen@gmail.com>
2017-12-20 23:31:01 +0200

    Remove redundant prop `disabled` (inherited from HTMLAttributes)

--- Switch/index.d.ts
+++ Switch/index.d.ts
@@ -1,7 +1,4 @@
 import MaterialComponent from '../MaterialComponent';
 import { VNode } from 'preact';
 
-declare interface ISwitchProps extends JSX.HTMLAttributes {
-  disabled?: boolean;
-}
-export default class Switch extends MaterialComponent<ISwitchProps, {}> {}
+export default class Switch extends MaterialComponent<JSX.HTMLAttributes, {}> {}
