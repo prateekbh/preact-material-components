e75b289801f68a50790d5f0976a4c8f68db8b4b3
Garrett Thompson <garrett@onovative.com>
2018-02-05 17:23:46 -0500

    Added "primary" & "secondary" props to Fab components (#554)
    * Added "primary" & "secondary" props to Fab components
    
    * Update TS definitions for Fab. Fix error where "class" or "className" prop could be overwritten internally
    
    * MaterialComponent handles adding "className" prop to element's "class" attribute. I don't need to handle it Fab.jsx. Setting both `class` and `className` in MaterialComponent was causing duplicate class names in the rendered markup

--- Fab/index.d.ts
+++ Fab/index.d.ts
@@ -4,9 +4,11 @@ import { VNode } from 'preact';
 declare interface IFabProps extends JSX.HTMLAttributes {
   'mini'?: boolean;
   'exited'?: boolean;
+  'primary'?: boolean;
+  'secondary'?: boolean
 }
 
 export default class Fab extends MaterialComponent<IFabProps, {}> {
   static Icon: typeof Icon;
 }
-export class Icon extends MaterialComponent<JSX.HTMLAttributes, {}> {}
+export class Icon extends MaterialComponent<JSX.HTMLAttributes, {}> { }
