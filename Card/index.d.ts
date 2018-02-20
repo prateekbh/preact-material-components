05765e878a6a13aed909edb2d5d9ed8985c20374
Prateek Bhatnagar <prateek89born@gmail.com>
2018-02-20 12:28:32 -0800

    Card fix (#591)
    * updating deps
    
    * textfield fixes
    
    * fixing cards
    
    * TS fixes
    
    * fixing ts
    
    * card fix
    
    * fixing TS declarations

--- Card/index.d.ts
+++ Card/index.d.ts
@@ -1,43 +1,29 @@
+import Button from '../Button';
+import Icon from '../Icon';
 import MaterialComponent from '../MaterialComponent';
 import { VNode } from 'preact';
-import Button from '../Button';
-
-declare class Section<PropsType = {}, StateType = {}> extends MaterialComponent<PropsType & JSX.HTMLAttributes, StateType> {}
 
 declare interface ICardProps extends JSX.HTMLAttributes {
-  'theme-dark'?: boolean;
+  'stroked'?: boolean;
 }
 export default class Card extends MaterialComponent<ICardProps, {}> {
-  static Primary: typeof Primary;
-  static SupportingText: typeof SupportingText;
   static Actions: typeof Actions;
-  static Action: typeof Action;
+  static ActionButton: typeof ActionButton;
+  static ActionIcon: typeof ActionIcon;
   static Media: typeof Media;
-  static Title: typeof Title;
-  static Subtitle: typeof Subtitle;
-  static HorizontalBlock: typeof HorizontalBlock;
-  static MediaItem: typeof MediaItem;
+  static CardMediaContent: typeof CardMediaContent;
 }
 
-declare class Primary extends Section<JSX.HTMLAttributes, {}> {}
-declare class SupportingText extends Section<JSX.HTMLAttributes, {}> {}
 declare interface IActionsProps extends JSX.HTMLAttributes {
-  vertical?: boolean;
+  "full-bleed"?: boolean;
 }
-declare class Actions extends Section<IActionsProps, {}> {}
-declare class Media extends Section<JSX.HTMLAttributes, {}> {}
 
-declare class Action extends Button<JSX.HTMLAttributes, {}> {}
+declare class Actions extends MaterialComponent<IActionsProps, {}> {}
+declare class ActionButton extends Button<JSX.HTMLAttributes, {}> {}
+declare class ActionIcon extends Icon<JSX.HTMLAttributes, {}> {}
+declare class CardMediaContent extends MaterialComponent<IActionsProps, {}> {}
 
-declare interface ITitleProps extends JSX.HTMLAttributes {
-  large?: boolean;
+declare interface IMediaProps extends JSX.HTMLAttributes {
+  x?: "square"|"16-9";
 }
-declare class Title extends MaterialComponent<ITitleProps, {}> {}
-
-declare class Subtitle extends MaterialComponent<JSX.HTMLAttributes, {}> {}
-declare class HorizontalBlock extends Section<JSX.HTMLAttributes, {}> {}
-
-declare interface IMediaItemProps extends JSX.HTMLAttributes {
-  x?: "1dot5"|"2"|"3";
-}
-declare class MediaItem extends MaterialComponent<IMediaItemProps, {}> {}
+declare class Media extends MaterialComponent<IMediaProps, {}> {}
