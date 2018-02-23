f36334b93b0e6eb64f2a190b393e0f2b4dee845c
Prateek Bhatnagar <prateek89born@gmail.com>
2018-02-22 16:39:21 -0800

    fixing ts definations

--- LineRipple/index.d.ts
+++ LineRipple/index.d.ts
@@ -1,4 +1,20 @@
 import MaterialComponent from '../MaterialComponent';
+import { MDCFoundation, MDCComponent } from '../MaterialComponentsWeb';
 import { VNode } from 'preact';
 
-export default class LineRipple extends MaterialComponent<JSX.HTMLAttributes, {}> {}
+export default class LineRipple extends MaterialComponent<JSX.HTMLAttributes, {}> {
+  MDComponent: MDCLineRipple;
+}
+
+declare class MDCLineRippleFoundation extends MDCFoundation<MDCLineRipple> {
+  activate(): void;
+  deactivate(): void;
+  setRippleCenter(xCoordinate: number): void;
+}
+
+declare class MDCLineRipple extends MDCComponent<MDCLineRippleFoundation> {
+  activate(): void;
+  deactivate(): void;
+  setRippleCenter(xCoordinate: number): void;
+  handleTransitionEnd(e: Event): void;
+}
