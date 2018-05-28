d6ff8c931d0304ba12654c008f155acba8610279
Antoine Colmard <a.colmard@gmail.com>
2018-05-28 02:31:45 +0200

    Fix Tabs within TabBarScroller initialized multiple times (#796)
    * :fire: Remove `indicator-accent` from `Tabs` component
    
    Remove `indicator-accent` from `Tabs` as it is no longer used (https://github.com/material-components/material-components-web/pull/1965)
    
    * Add TabBarScrollerTabs component
    
    - `TabBarScrollerTabs` should be used within `TabBarScroller` instead of `Tabs`.
    
    - Component name is inspired by class name used by @material/mdc-tab: '.mdc-tab-bar-scroller__scroll-frame__tabs'
    
    - `scroller` property is no longer needed on `Tabs` and allows a simpler implementation
    
    - `activeTabIndex` can be used on `Tabs` and `TabBarScroller` to select active tab index
    
    * Fix prettier formatting
    
    * Uptable tabs golden screenshot

--- Tabs/index.d.ts
+++ Tabs/index.d.ts
@@ -3,22 +3,37 @@ import {VNode} from 'preact';
 import {MDCFoundation, MDCComponent} from '../MaterialComponentsWeb';
 
 declare interface ITabsProps extends JSX.HTMLAttributes {
-  'indicator-accent'?: boolean;
   'icon-tab-bar'?: boolean;
   'icons-with-text'?: boolean;
-  scroller?: boolean;
+  activeTabIndex?: number;
 }
 export default class Tabs extends MaterialComponent<ITabsProps, {}> {
   static TabBarScroller: typeof TabBarScroller;
+  static TabBarScrollerTabs: typeof TabBarScrollerTabs;
   static Tab: typeof Tab;
   static TabIconLabel: typeof TabIconLabel;
 
   MDComponent: MDCTabBar;
 }
 
-declare class TabBarScroller extends MaterialComponent<JSX.HTMLAttributes, {}> {
+declare interface ITabBarScrollerProps extends JSX.HTMLAttributes {
+  activeTabIndex?: number;
+}
+declare class TabBarScroller extends MaterialComponent<
+  ITabBarScrollerProps,
+  {}
+> {
   MDComponent: MDCTabBarScroller;
 }
+
+declare interface ITabBarScrollerTabsProps extends JSX.HTMLAttributes {
+  'icon-tab-bar'?: boolean;
+  'icons-with-text'?: boolean;
+}
+declare class TabBarScrollerTabs extends MaterialComponent<
+  ITabBarScrollerTabsProps,
+  {}
+> {}
 
 interface ITabProps extends JSX.HTMLAttributes {
   active?: boolean;
--- docs/src/routes/tabs/index.js
+++ docs/src/routes/tabs/index.js
@@ -12,7 +12,6 @@ import CodeBlock from '../../components/code-block';
 // Samples
 import sample from './sample.txt';
 import defaultSample from './default-sample.txt';
-import accentSample from './accent-sample.txt';
 import iconTabsSample from './icon-tabs-sample.txt';
 import textTabsSample from './text-tabs-sample.txt';
 import scrollableSample from './scrollable-sample.txt';
@@ -26,20 +25,12 @@ export default class TabsPage extends Component {
         component: 'Tabs',
         props: [
           {
-            name: 'indicator-accent',
-            description: 'Makes the indicator accented.'
-          },
-          {
             name: 'icon-tab-bar',
             description: 'Makes the tab bar accomodate icons.'
           },
           {
             name: 'icons-with-text',
             description: 'Makes the tab bar accomodate icons and label.'
-          },
-          {
-            name: 'scroller',
-            description: 'Makes the tabs scrollable.'
           },
           {
             name: 'activeTabIndex',
@@ -54,6 +45,29 @@ export default class TabsPage extends Component {
           {
             name: 'active',
             description: 'Sets the tab active along with `activeTabIndex` prop.'
+          }
+        ]
+      },
+      {
+        component: 'TabBarScroller',
+        props: [
+          {
+            name: 'activeTabIndex',
+            description:
+              'Sets the bottom bar of active tab, also please use `active` prop on tab.'
+          }
+        ]
+      },
+      {
+        component: 'TabBarScrollerTabs',
+        props: [
+          {
+            name: 'icon-tab-bar',
+            description: 'Makes the tab bar accomodate icons.'
+          },
+          {
+            name: 'icons-with-text',
+            description: 'Makes the tab bar accomodate icons and label.'
           }
         ]
       }
@@ -88,15 +102,6 @@ export default class TabsPage extends Component {
         </Tabs>
         <CodeBlock>
           <code class="lang-js">{defaultSample}</code>
-        </CodeBlock>
-        <div className="mdc-typography--title">With indicator accent </div>
-        <Tabs className="demo-tabs" indicator-accent={true}>
-          <Tabs.Tab>Tab1</Tabs.Tab>
-          <Tabs.Tab>Tab2</Tabs.Tab>
-          <Tabs.Tab>Tab3</Tabs.Tab>
-        </Tabs>
-        <CodeBlock>
-          <code class="lang-js">{accentSample}</code>
         </CodeBlock>
         <div className="mdc-typography--title">Icons tabs bar </div>
         <Tabs className="demo-tabs" icon-tab-bar={true}>
@@ -133,7 +138,7 @@ export default class TabsPage extends Component {
         </CodeBlock>
         <div className="mdc-typography--title">Scrollable tabs </div>
         <Tabs.TabBarScroller>
-          <Tabs scroller={true}>
+          <Tabs.TabBarScrollerTabs>
             <Tabs.Tab>tab1</Tabs.Tab>
             <Tabs.Tab active={true}>tab2</Tabs.Tab>
             <Tabs.Tab>tab3</Tabs.Tab>
@@ -145,7 +150,7 @@ export default class TabsPage extends Component {
             <Tabs.Tab>tab9</Tabs.Tab>
             <Tabs.Tab>tab10</Tabs.Tab>
             <Tabs.Tab>tab11</Tabs.Tab>
-          </Tabs>
+          </Tabs.TabBarScrollerTabs>
         </Tabs.TabBarScroller>
         <CodeBlock>
           <code class="lang-js">{scrollableSample}</code>
