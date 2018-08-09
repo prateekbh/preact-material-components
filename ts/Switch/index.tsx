b40331594e2b64b3d0416b6d126aa3a36c8bfd3c
Prateek Bhatnagar <prateek89born@gmail.com>
2018-08-09 12:56:02 -0700

    Changes for38 (#923)
    * fixing list and switch
    
    * implementing tab-bar
    
    * bug fix
    
    * Sync with Prettier
    
    * re-order
    
    * Fixed style
    
    * resolving comments
    
    * adding css file
    
    * Sync with Prettier
    
    * fixing golden tests
    
    * fixing golden test

--- ts/Switch/index.tsx
+++ ts/Switch/index.tsx
@@ -1,3 +1,4 @@
+import {MDCSwitch} from '@material/switch';
 import autobind from 'autobind-decorator';
 import {h} from 'preact';
 import MaterialComponent from '../Base/MaterialComponent';
@@ -11,19 +12,38 @@ export interface ISwitchState {}
 export class Switch extends MaterialComponent<ISwitchProps, ISwitchState> {
   protected componentName = 'switch';
   protected mdcProps = ['disabled'];
+  protected MDComponent?: MDCSwitch;
+
+  public componentDidMount() {
+    super.componentDidMount();
+    if (this.control) {
+      this.MDComponent = new MDCSwitch(this.control);
+    }
+  }
+
+  public componentWillUnmount() {
+    super.componentWillUnmount();
+    if (this.MDComponent) {
+      this.MDComponent.destroy();
+    }
+  }
 
   @autobind
   protected materialDom(allprops) {
     const {className, ...props} = allprops;
     return (
-      <div className={className}>
-        <input
-          type="checkbox"
-          className="mdc-switch__native-control"
-          {...props}
-        />
-        <div className="mdc-switch__background">
-          <div className="mdc-switch__knob" />
+      <div className={className} ref={this.setControlRef}>
+        <div class="mdc-switch__track" />
+        <div class="mdc-switch__thumb-underlay">
+          <div class="mdc-switch__thumb">
+            <input
+              type="checkbox"
+              id="basic-switch"
+              class="mdc-switch__native-control"
+              role="switch"
+              {...props}
+            />
+          </div>
         </div>
       </div>
     );
