e532b5a32c6b9d401c13f9fca3cedc9c9c70e68a
Prateek Bhatnagar <prateek89born@gmail.com>
2018-09-17 11:51:08 -0700

    lint fixes

--- ts/IconButton/index.tsx
+++ ts/IconButton/index.tsx
@@ -1,6 +1,6 @@
-import MaterialComponent from '../Base/MaterialComponent';
-import {h} from 'preact';
 import {MDCIconButtonToggle} from '@material/icon-button';
+import {h} from 'preact';
+import MaterialComponent from '../Base/MaterialComponent';
 
 export interface IIconButtonProps {
   disabled?: boolean;
