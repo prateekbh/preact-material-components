3c0b6e636b3231691c6faa070fbe17aba50fbd7d
Prateek Bhatnagar <prateek89born@gmail.com>
2018-09-17 09:34:02 -0700

    Update index.tsx

--- ts/IconButton/index.tsx
+++ ts/IconButton/index.tsx
@@ -1,6 +1,6 @@
-import {h} from 'preact';
-import {MDCIconButtonToggle} from '@material/icon-button';
 import MaterialComponent from '../Base/MaterialComponent';
+import {MDCIconButtonToggle} from '@material/icon-button';
+import {h} from 'preact';
 
 export interface IIconButtonProps {
   disabled?: boolean;
