0f5e05116eb7adbc980f388ab4a30cc76f8a3bbb
Robbert Wethmar <robbert@digitalica.nl>
2018-12-18 18:11:12 +0100

    fix(docs) for prettierci

--- docs/src/components/app/index.js
+++ docs/src/components/app/index.js
@@ -218,7 +218,7 @@ export default class Home extends Component {
     } else {
       document.body.classList.remove('mdc-theme--dark');
     }
-  }
+  };
 
   handleRoute_ = url => {
     this.setState({
