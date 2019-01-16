e845d4b719c80bc2ce72875fee30018ff3df35e4
Cromefire_ <tim.langhorst@outlook.de>
2019-01-16 18:45:15 +0100

    Fixed sass imports
    Signed-off-by: Cromefire_ <tim.langhorst@outlook.de>

--- packages/docs/preact.config.js
+++ packages/docs/preact.config.js
@@ -1,15 +1,26 @@
-const path = require('path');
+import {resolve} from 'path';
+import {readdirSync, existsSync} from 'fs';
 
-export default config => {
-  const paths = [
-    path.resolve(__dirname, '..', 'drawer', 'node_modules'),
-    path.resolve(__dirname, '..', 'checkbox', 'node_modules'),
-    path.resolve(__dirname, '..', 'tab', 'node_modules'),
-    path.resolve(__dirname, '..', 'textfield', 'node_modules'),
+/**
+ * Function that mutates original webpack config.
+ * Supports asynchronous changes when promise is returned.
+ *
+ * @param {object} config - original webpack config.
+ * @param {object} env - options passed to CLI.
+ * @param {WebpackConfigHelpers} helpers - object with useful helpers when working with config.
+ **/
+export default function(config, env, helpers) {
+  // TODO: Build custom resolver
+  const includeDirs = readdirSync('..', {withFileTypes: true})
+    .filter(file => file.isDirectory())
+    .map(dir => resolve('..', dir.name, 'node_modules'))
+    .filter(full => existsSync(full));
 
-  ];
-  config.module.rules[2].use[0].options.options.includePaths.push(
-    ...paths
-  );
-  return config;
+  for (const {loader} of helpers.getLoadersByName(config, 'proxy-loader')) {
+    if (loader.options && loader.options.options && loader.options.loader) {
+      if (loader.options.loader === 'sass-loader') {
+        loader.options.options.includePaths.push(...includeDirs);
+      }
+    }
+  }
 }
