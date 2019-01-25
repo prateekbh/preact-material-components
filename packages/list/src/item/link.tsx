fabd799ad547f9a87b171368a09e6f0965d88976
Cromefire_ <tim.langhorst@outlook.de>
2019-01-25 18:35:23 +0100

    Fixed loading via sass side effects
    Signed-off-by: Cromefire_ <tim.langhorst@outlook.de>

--- packages/list/src/item/link.tsx
+++ packages/list/src/item/link.tsx
@@ -1,16 +1,22 @@
 import {MaterialComponent} from '@preact-material-components/base/lib/MaterialComponent';
 import {h} from 'preact';
+import {SoftMerge} from '@preact-material-components/base/lib/types';
 
-export interface IListItemLinkProps {}
+export interface IListItemLinkProps {
+  activated?: boolean;
+}
 
 export interface IListItemLinkState {}
 
 export class ListItemLink<
   PropTypes = {},
   StateTypes = {}
-> extends MaterialComponent<PropTypes, StateTypes> {
+> extends MaterialComponent<
+  SoftMerge<PropTypes & IListItemLinkProps, JSX.HTMLAttributes>,
+  StateTypes & IListItemLinkState
+> {
   protected componentName = 'list-item';
-  protected mdcProps = [];
+  protected mdcProps = ['activated'];
 
   protected materialDom(props) {
     return (
