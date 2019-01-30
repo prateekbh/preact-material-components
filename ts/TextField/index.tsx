prateekbh/preact-material-components

commit bc2125ceb27f14b58f42b6eeb54351b216fca542

Remove unneeded argument to render for TextField (#1045)
Signed-off-by: Cromefire_ <tim.l@nghorst.net>

ts/TextField/index.tsx
@@ -260,7 +260,7 @@ export class TextField extends Component<ITextFieldProps, ITextFieldState> {
     });
   }
 
-  public render(allprops, {showFloatingLabel}) {
+  public render(allprops) {
     const {
       className,
       outerStyle,

.gitignore
@@ -64,3 +64,4 @@ esm
 !/docs/prettier.config.js
 tests/bin/
 tests/golden/dom/
+/preact-material-components-v*.*.*.tgz

commit 0f772ea2f2d8afc508c5046e5d40348715561bcc

release docs for 1.5.6 (#1275)
* dummy release (#1145)

* dummy release

* fixing screen shot

* fixing serve

* fix server

* Sync with Prettier

* fixes screenshot

* linux pics

* linux pics

* Fixed the select onchange method typings (#1152)

* Fixed the select onchange method typings

selectedIndex is provided on e.target.selectedIndex, but the typings said it should be accessed through e.selectedIndex, thus not compiling when accessing e.target.selectedIndex

* Update index.tsx

* Published 1.5.5 (#1158)

* Published 1.5.5

* Fixed version in package-lock.json

* Switch: Use mdcNotifyProps (#1190)

* Avoid re-setting input.value when already set (#1193)

* Avoid re-setting input.value when already set

* prettier

* Update index.tsx

* Update index.tsx

* Update index.tsx

* fix(docs): typo in drawer doc (#1196)

* fix(docs): make dark mode toggle work again (#1197)

* fix(docs): make dark mode toggle work again
(broken in commit bc2125ceb27f14b58f42b6eeb54351b216fca542)

* fix(docs) for prettierci

* fix(docs): add DRYness to docs (and minor fix in textfield link) (#1198)

* fix(docs): add DRYness to docs (and minor fix in textfield link)

* fix(docs): header in new Original Documentation fixed and minor ajustments in some tests

* fix(docs): make prettier happy and fix golden image for card

* fix(docs): new url (little less DRY)

* fix(docs): better div structure

* add test artifact to .gitignore

* fix(docs): prettier is happier now

* fix(docs): better div structure

* fix(docs): separate samplecodeblock component

* prettier

* fix test, by accepting travis generated textbox screendump

* Update clean-webpack-plugin to the latest version 🚀 (#1153)

* chore(package): update clean-webpack-plugin to version 1.0.0

* chore(package): update lockfile package-lock.json

* [Greenkeeper] Ignore material packages (#1235)

* [Greenkeeper] Ignore material packages

ts/TextField/index.tsx
@@ -111,6 +111,7 @@ export class TextFieldInput extends MaterialComponent<
   public componentWillReceiveProps(nextProps: ITextFieldInputProps) {
     super.componentWillReceiveProps(nextProps);
     if (
+      this.MDComponent &&
       nextProps.value &&
       this.props.value !== nextProps.value &&
       this.MDComponent.value !== nextProps.value