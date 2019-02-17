ccfb45f7e9c03eeb403e6ab6b256ee93f1d5b9e7
Cromefire_ <tim.langhorst@outlook.de>
2019-02-17 14:26:55 +0100

    Improved buildscript
    Signed-off-by: Cromefire_ <tim.langhorst@outlook.de>

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -3,14 +3,14 @@ import * as npx from 'libnpx';
 import {join, resolve} from 'path';
 import {ls} from 'shelljs';
 
-const rootDir = resolve(join(__dirname, '..'));
-const pkgDir = join(rootDir, 'packages');
-
-function project2path(proj) {
-  return resolve(join(pkgDir, proj));
-}
-
 if (require.main === module) {
+  const rootDir = resolve(join(__dirname, '..'));
+  const pkgDir = join(rootDir, 'packages');
+
+  function project2path(proj) {
+    return resolve(join(pkgDir, proj));
+  }
+
   const packages = ls(pkgDir);
 
   console.info('Collecting packages...');
@@ -19,28 +19,17 @@ if (require.main === module) {
     return existsSync(tsconfigPath);
   });
 
+  const args = ['--build'];
   if (process.argv.includes('--watch')) {
-    console.info('Entering watch mode....');
-    const args = ['--watch', '-b'];
-
-    const tsProjects: string[] = [];
-    tsPackages.forEach(proj => tsProjects.push(project2path(proj)));
-
-    npx({
-      cmdOpts: args.concat(tsProjects),
-      command: 'tsc',
-      package: ['typescript']
-    });
-  } else {
-    const args = ['-b'];
-
-    const tsProjects: string[] = [];
-    tsPackages.forEach(proj => tsProjects.push(project2path(proj)));
-
-    npx({
-      cmdOpts: args.concat(tsProjects),
-      command: 'tsc',
-      package: ['typescript']
-    });
+    args.push('--watch');
   }
+
+  const tsProjects: string[] = [];
+  tsPackages.forEach(proj => tsProjects.push(project2path(proj)));
+
+  npx({
+    cmdOpts: args.concat(tsProjects),
+    command: 'tsc',
+    package: ['typescript']
+  });
 }
