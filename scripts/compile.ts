a338c2f941cd7f25e64fc4a9faf6c075054c89e4
Cromefire_ <tim.langhorst@outlook.de>
2019-03-06 14:21:55 +0100

    Upgraded os
    Signed-off-by: Cromefire_ <tim.langhorst@outlook.de>

--- scripts/compile.ts
+++ scripts/compile.ts
@@ -3,14 +3,14 @@ import * as npx from 'libnpx';
 import {join, resolve} from 'path';
 import {ls} from 'shelljs';
 
+const rootDir = resolve(join(__dirname, '..'));
+const pkgDir = join(rootDir, 'packages');
+
+function project2path(proj) {
+  return resolve(join(pkgDir, proj));
+}
+
 if (require.main === module) {
-  const rootDir = resolve(join(__dirname, '..'));
-  const pkgDir = join(rootDir, 'packages');
-
-  function project2path(proj) {
-    return resolve(join(pkgDir, proj));
-  }
-
   const packages = ls(pkgDir);
 
   console.info('Collecting packages...');
