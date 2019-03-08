d6580f6f9e138115c8b6434640499fd3ee7f35ca
Prateek Bhatnagar <prateek89born@gmail.com>
2019-03-07 16:03:03 -0800

    more script

--- packages/docs/test-runner.js
+++ packages/docs/test-runner.js
@@ -1,5 +1,10 @@
 const shelljs = require('shelljs');
-
-console.log(shelljs.env['$TRAVIS_PULL_REQUEST']);
+console.log(shelljs.env);
+console.log(shelljs.env['TRAVIS_PULL_REQUEST']);
+if (shelljs.env['TRAVIS_PULL_REQUEST'] != 'false') {
+  const commitRange = `FETCH_HEAD..${shelljs.env['TRAVIS_BRANCH']}`;
+  console.log('git diff');
+  console.log(shelljs.exec(`git --no-pager diff --name-only ${commitRange}`));
+}
 
 process.exit(1);
