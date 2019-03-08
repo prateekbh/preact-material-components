6a9621de7d497cc4d61616f8d7124704fea0813a
Prateek Bhatnagar <prateek89born@gmail.com>
2019-03-08 11:00:07 -0800

    Test runner (#1297)
    * selective test runner
    
    * more script
    
    * get file names
    
    * adding base branch
    
    * debug prints
    
    * different scenerio for PR and push testing
    
    * different scenerio for PR and push testing
    
    * fixing fetch head
    
    * using travis's commit range
    
    * --amend
    
    * fixing files list
    
    * echo files changed
    
    * Only unique packages
    
    * Update test-runner.js
    
    * selective tests running
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update README.md
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update test-runner.js
    
    * Update package.json
    
    * Update README.md
    
    * Update test-runner.js
    
    * fixing lint
    
    * removing unwanted deps

---
+++ packages/docs/test-runner.js
@@ -0,0 +1,46 @@
+const shelljs = require('shelljs');
+
+if (shelljs.env['TRAVIS_PULL_REQUEST'] !== 'false') {
+  const packageRegexp = /packages\/([a-z]*)\/.*/;
+  console.log(
+    `cmd: git --no-pager diff --name-only ${shelljs.env['TRAVIS_COMMIT_RANGE']}`
+  );
+  const {stderr, stdout} = shelljs.exec(
+    `git --no-pager diff --name-only ${shelljs.env['TRAVIS_COMMIT_RANGE']}`
+  );
+  if (stderr) {
+    console.log(stderr);
+    process.exit(-1);
+  }
+  const files = stdout.split('\n');
+  const packagesChanged = new Set(
+    files
+      .filter(file => file.match(packageRegexp))
+      .map(pckg => pckg.match(packageRegexp)[1])
+  );
+  // remove docs from this set as it is does not have a test for itself.
+  packagesChanged.delete('docs');
+  console.log(
+    `Detected changes in packages: ${[...packagesChanged.values()].join(', ')}`
+  );
+  if (packagesChanged.size === 0) {
+    console.log(
+      'No changes in packages found, not running visual regression testing.'
+    );
+    process.exit(0);
+  } else if (packagesChanged.has('base')) {
+    // if base has changed it might impact every component
+    // so run all of them.
+    const {code} = shelljs.exec('cypress run');
+    process.exit(code);
+  } else {
+    // run the tests for the packages which changed
+    const specs = [...packagesChanged.values()].map(
+      pkg => `cypress/integration/visual-test/${pkg}-test.spec.js`
+    );
+    const {code} = shelljs.exec(`cypress run --spec ${specs.join(',')}`);
+    process.exit(code);
+  }
+}
+
+process.exit(0);
