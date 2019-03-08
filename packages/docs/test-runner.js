0dcadfa1b94eb9bca480ec29750a54aa21418d8e
Prateek Bhatnagar <prateek89born@gmail.com>
2019-03-07 17:10:15 -0800

    fixing files list

--- packages/docs/test-runner.js
+++ packages/docs/test-runner.js
@@ -6,11 +6,15 @@ if (shelljs.env['TRAVIS_PULL_REQUEST'] !== 'false') {
   console.log(
     `cmd: git --no-pager diff --name-only ${shelljs.env['TRAVIS_COMMIT_RANGE']}`
   );
-  const list = shelljs.exec(
+  const {stderr, stdout} = shelljs.exec(
     `git --no-pager diff --name-only ${shelljs.env['TRAVIS_COMMIT_RANGE']}`
   );
-  console.log(list);
-  const packagesChanged = list
+  if (stderr) {
+    console.log(stderr);
+    process.exit(-1);
+  }
+  const files = stdout.split('\n');
+  const packagesChanged = files
     .filter(file => file.match(packageRegexp))
     .map(pckg => pckg.match(packageRegexp)[1]);
 }
