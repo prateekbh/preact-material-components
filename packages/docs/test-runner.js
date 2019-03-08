73bbde63d81e55c30c3b6cb493a2c9cb560aea47
Prateek Bhatnagar <prateek89born@gmail.com>
2019-03-07 17:02:11 -0800

    --amend

--- packages/docs/test-runner.js
+++ packages/docs/test-runner.js
@@ -4,10 +4,10 @@ console.log('=================');
 if (shelljs.env['TRAVIS_PULL_REQUEST'] !== 'false') {
   const packageRegexp = /packages\/([a-z]*)\/.*/;
   console.log(
-    `cmd: git --no-pager diff --name-only ${shell.env['TRAVIS_COMMIT_RANGE']}`
+    `cmd: git --no-pager diff --name-only ${shelljs.env['TRAVIS_COMMIT_RANGE']}`
   );
   const list = shelljs.exec(
-    `git --no-pager diff --name-only ${shell.env['TRAVIS_COMMIT_RANGE']}`
+    `git --no-pager diff --name-only ${shelljs.env['TRAVIS_COMMIT_RANGE']}`
   );
   console.log(list);
   const packagesChanged = list
