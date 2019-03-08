13c6c8dac6d3a6c008e87f2f9f2d3253a0a51ee7
Prateek Bhatnagar <prateek89born@gmail.com>
2019-03-07 16:53:29 -0800

    using travis's commit range

--- packages/docs/test-runner.js
+++ packages/docs/test-runner.js
@@ -1,11 +1,14 @@
 const shelljs = require('shelljs');
 console.log(shelljs.env);
 console.log('=================');
-const commitRange = `FETCH_HEAD..${shelljs.env['TRAVIS_BRANCH']}`;
 if (shelljs.env['TRAVIS_PULL_REQUEST'] !== 'false') {
   const packageRegexp = /packages\/([a-z]*)\/.*/;
-  console.log(`cmd: git --no-pager diff --name-only ${commitRange}`);
-  const list = shelljs.exec(`git --no-pager diff --name-only ${commitRange}`);
+  console.log(
+    `cmd: git --no-pager diff --name-only ${shell.env['TRAVIS_COMMIT_RANGE']}`
+  );
+  const list = shelljs.exec(
+    `git --no-pager diff --name-only ${shell.env['TRAVIS_COMMIT_RANGE']}`
+  );
   console.log(list);
   const packagesChanged = list
     .filter(file => file.match(packageRegexp))
