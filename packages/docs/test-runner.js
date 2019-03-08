0387880d49e8a77a52912267a4303ca9ca0cefaa
Prateek Bhatnagar <prateek89born@gmail.com>
2019-03-07 16:38:37 -0800

    different scenerio for PR and push testing

--- packages/docs/test-runner.js
+++ packages/docs/test-runner.js
@@ -3,11 +3,17 @@ console.log(shelljs.env);
 console.log('=================');
 const head = shelljs.env['FETCH_HEAD'] || '2.0';
 const commitRange = `${head}..${shelljs.env['TRAVIS_BRANCH']}`;
-const packageRegexp = /packages\/([a-z]*)\/.*/;
-console.log(`cmd: git --no-pager diff --name-only ${commitRange}`);
-const list = shelljs.exec(`git --no-pager diff --name-only ${commitRange}`);
-const packagesChanged = list
-  .filter(file => file.match(packageRegexp))
-  .map(pckg => pckg.match(packageRegexp)[1]);
+if (shelljs.env['TRAVIS_PULL_REQUEST'] === 'false') {
+  // run all tests
+  shelljs.exec('cypress run');
+} else {
+  const packageRegexp = /packages\/([a-z]*)\/.*/;
+  console.log(`cmd: git --no-pager diff --name-only ${commitRange}`);
+  const list = shelljs.exec(`git --no-pager diff --name-only ${commitRange}`);
+  console.log(list);
+  const packagesChanged = list
+    .filter(file => file.match(packageRegexp))
+    .map(pckg => pckg.match(packageRegexp)[1]);
+}
 
 process.exit(1);
