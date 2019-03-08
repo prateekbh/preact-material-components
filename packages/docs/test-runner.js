f7d482188d40ca075449ecf17a1db3291d0ba6f8
Prateek Bhatnagar <prateek89born@gmail.com>
2019-03-07 23:34:12 -0800

    Update test-runner.js

--- packages/docs/test-runner.js
+++ packages/docs/test-runner.js
@@ -35,7 +35,9 @@ if (shelljs.env['TRAVIS_PULL_REQUEST'] !== 'false') {
     console.log(shelljs.pwd())
     // run the tests for the packages which changed
     const {code} = shelljs.exec(
-      `cypress run --spec cypress/integration/visual-test/but*-spec.test.js`);
+      `cypress run --spec cypress/integration/visual-test/{${[
+        ...packagesChanged.values()
+      ].join(',')}}-test.spec.js`);
     console.log(code);
     process.exit(code);
   }
