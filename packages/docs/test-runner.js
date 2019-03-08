c90faea36433d70a04621deb038c2c353d73dcfe
Prateek Bhatnagar <prateek89born@gmail.com>
2019-03-07 16:08:30 -0800

    get file names

--- packages/docs/test-runner.js
+++ packages/docs/test-runner.js
@@ -1,10 +1,8 @@
 const shelljs = require('shelljs');
 console.log(shelljs.env);
-console.log(shelljs.env['TRAVIS_PULL_REQUEST']);
-if (shelljs.env['TRAVIS_PULL_REQUEST'] != 'false') {
-  const commitRange = `FETCH_HEAD..${shelljs.env['TRAVIS_BRANCH']}`;
-  console.log('git diff');
-  console.log(shelljs.exec(`git --no-pager diff --name-only ${commitRange}`));
-}
+console.log('=================');
+const commitRange = `FETCH_HEAD..${shelljs.env['TRAVIS_BRANCH']}`;
+console.log('git diff');
+const list = shelljs.exec(`git --no-pager diff --name-only ${commitRange}`);
 
 process.exit(1);
