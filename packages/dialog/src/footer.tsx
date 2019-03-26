672eeeed9366b9e0d1a6938e6eb5c83e495f23b9
Prateek Bhatnagar <prateek89born@gmail.com>
2019-03-25 23:40:17 -0700

    Update footer.tsx

--- packages/dialog/src/footer.tsx
+++ packages/dialog/src/footer.tsx
@@ -1,5 +1,5 @@
 import {MaterialComponent} from '@preact-material-components/base/lib/MaterialComponent';
-import bind from 'bind-decorator';
+
 import {h} from 'preact';
 
 export interface IDialogFooterProps {}
@@ -13,7 +13,7 @@ export class DialogFooter extends MaterialComponent<
   protected componentName = 'dialog__footer';
   protected mdcProps = [];
 
-  @bind
+
   protected materialDom(props) {
     return <footer {...props}>{props.children}</footer>;
   }
