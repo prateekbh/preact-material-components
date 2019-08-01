3511d138fa07797bbb96ad4a72513c9e7517a059
Cromefire_ <tim.langhorst@outlook.de>
2019-08-02 00:25:16 +0200

    Added ripple test
    Signed-off-by: Cromefire_ <tim.langhorst@outlook.de>

---
+++ packages/docs/cypress/integration/visual-test/ripple-test.spec.js
@@ -0,0 +1,12 @@
+import {testScreenshot, baseURL} from '../../utils/utils';
+
+describe('Visual  Regression Tests', () => {
+  const url = `${baseURL}/component/ripple`;
+  beforeEach(() => {
+    cy.visit(url);
+  });
+  afterEach(() => {
+    cy.eyesClose();
+  });
+  testScreenshot('ripplePage');
+});
