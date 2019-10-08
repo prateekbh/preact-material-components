beaff7207b58d7a56686dea0866052966e941da8
Pina <fopina@gmail.com>
2019-10-08 23:16:20 +0100

    add support for non-icon List ItemMeta #1337

--- packages/list/src/item/meta.tsx
+++ packages/list/src/item/meta.tsx
@@ -1,4 +1,5 @@
 import {h} from 'preact';
+import {MaterialComponent} from '@preact-material-components/base/lib/MaterialComponent';
 import {
   IListItemGraphicProps,
   IListItemGraphicState,
@@ -11,4 +12,23 @@ export interface IListItemMetaState extends IListItemGraphicState {}
 
 export class ListItemMeta extends ListItemGraphic {
   protected componentName = 'list-item__meta';
+}
+
+export interface IListItemMetaTextProps {}
+export interface IListItemMetaTextState {}
+
+export class ListItemMetaText extends MaterialComponent<
+  IListItemMetaTextProps,
+  IListItemMetaTextState
+> {
+  protected componentName = 'list-item__meta';
+  protected mdcProps = [];
+
+  protected materialDom(props) {
+    return (
+      <span {...props} ref={this.setControlRef} role="presentation">
+        {props.children}
+      </span>
+    );
+  }
 }
